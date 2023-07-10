ed7cba674d659d8330cd314f9f5ca9b5b8a46768
Denny_Kristian <133180420+denny2911@users.noreply.github.com>
2023-07-10 20:29:32 +0700

    Add files via upload
    Login

--- signin.dart
+++ signin.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:panjiartamasinaga/kelompok/floating.dart';
-import 'package:panjiartamasinaga/kelompok/register.dart';
+import 'package:flutter_application_2/pertemuan12/floating.dart';
+import 'package:flutter_application_2/pertemuan12/home.dart';
+import 'package:flutter_application_2/pertemuan12/register.dart';
 
 class Signin extends StatefulWidget {
   const Signin({super.key});
+
   @override
   State<Signin> createState() => _SigninState();
 }
 
 class _SigninState extends State<Signin> {
-  final _formfield = GlobalKey<FormState>;
-  final emailController = TextEditingController();
-  final passController = TextEditingController();
-  bool passToggle = true;
-
   String itemSelected = 'User';
   List items = ['User', 'Penyedia Jasa'];
   @override
@@ -44,66 +41,51 @@ class _SigninState extends State<Signin> {
                   child: Column(
                     children: [
                       Container(
-                          width: 300,
-                          child: Container(
-                            child: Container(
-                              width: 280,
-                              child: Column(
-                                children: [
-                                  TextFormField(
-                                    keyboardType: TextInputType.emailAddress,
-                                    controller: emailController,
-                                    decoration: InputDecoration(
-                                        labelText: "Username",
-                                        prefixIcon: Icon(Icons.people)),
-                                  ),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  TextFormField(
-                                    keyboardType: TextInputType.emailAddress,
-                                    controller: passController,
-                                    obscureText: passToggle,
-                                    decoration: InputDecoration(
-                                      labelText: "Password",
-                                      prefixIcon: Icon(Icons.lock),
-                                      suffix: InkWell(
-                                        onTap: () {
-                                          setState(() {
-                                            passToggle = !passToggle;
-                                          });
-                                        },
-                                        child: Icon(passToggle
-                                            ? Icons.visibility
-                                            : Icons.visibility_off),
-                                      ),
-                                    ),
-                                  )
-                                ],
+                        margin: EdgeInsets.only(left: 30),
+                        width: 300,
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                label: Text('Username',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black)),
                               ),
                             ),
-                          ))
+                            TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                label: Text('Password',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 30),
+                        width: 300,
+                        child: DropdownButton(
+                          items: items.map((item) {
+                            return DropdownMenuItem(
+                              child: Row(children: [Text(item)]),
+                              value: item,
+                            );
+                          }).toList(),
+                          value: itemSelected,
+                          onChanged: (val) {
+                            setState(() {
+                              itemSelected = val as String;
+                            });
+                          },
+                        ),
+                      ),
                     ],
                   )),
-              SizedBox(height: 10),
-              Container(
-                margin: EdgeInsets.only(left: 7),
-                width: 300,
-                child: DropdownButtonFormField(
-                  items: items.map((item) {
-                    return DropdownMenuItem(
-                      child: Row(children: [Text(item)]),
-                      value: item,
-                    );
-                  }).toList(),
-                  value: itemSelected,
-                  onChanged: (val) {
-                    setState(() {
-                      itemSelected = val as String;
-                    });
-                  },
-                ),
-              ),
               SizedBox(height: 10),
               Container(
                 margin: EdgeInsets.only(left: 200),
@@ -117,10 +99,8 @@ class _SigninState extends State<Signin> {
                 width: 100,
                 child: ElevatedButton(
                     onPressed: () {
-                      if (passController == null)
-                        SnackBar(
-                          content: Text('Yay! A SnackBar!'),
-                        );
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Floating()));
                     },
                     child: Text('Login')),
               ),
